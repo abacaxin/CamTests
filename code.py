fa0a249926a40c92c2fe8c41a5af5b637b2c6b14
Baqui <87232170+abacaxin@users.noreply.github.com>
2026-04-08 13:11:21 -0300

    Refactor video capture loop for better readability

--- code.py
+++ code.py
@@ -5,20 +5,21 @@ categorias = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
 modelo = cv2.dnn.readNetFromONNX('modelo.onnx')
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FPS, 10)
-try: while True:
-    ret, frame = cap.read()
+try: 
+    while True:
+        ret, frame = cap.read()
+        
+        if not ret: exit()
+        blob = cv2.dnn.blobFromImage(frame, 1/255.0, (224, 224), swapRB=True, crop=False)
+        modelo.setInput(blob)
+        saida = modelo.forward()
+        indice = np.argmax(saida[0])
     
-    if not ret: exit()
-    blob = cv2.dnn.blobFromImage(frame, 1/255.0, (224, 224), swapRB=True, crop=False)
-    modelo.setInput(blob)
-    saida = modelo.forward()
-    indice = np.argmax(saida[0])
-
-    cv2.putText(frame, f"{categorias[indice]} ({saida[0][indice]:.1%})", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.imshow('Classificação de Resíduos', frame)
-
-    if cv2.waitKey(1) == 27:
-        break
+        cv2.putText(frame, f"{categorias[indice]} ({saida[0][indice]:.1%})", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.imshow('Classificação de Resíduos', frame)
+    
+        if cv2.waitKey(1) == 27:
+            break
 
 finally:
     cap.release()   
